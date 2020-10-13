27494b35b809d2a3d4f4f7c9fc9e2e0540677062
bendoflex <joanzaf@lilo.org>
2020-10-14 00:36:14 +0200

    constructor passed to game logic

--- src/components/game.js
+++ src/components/game.js
@@ -5,7 +5,14 @@ import Board from "./board"
 
 class Game extends React.Component {
   
-
+  constructor(props){
+    super(props)
+    this.state = {
+      history : {
+        squares : Array(9).fill(null)
+      }
+    }
+  }
  
 
   render() {
